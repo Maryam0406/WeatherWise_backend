b3e9d384f8a8203991a4fb28d7e90b7bb6c12283
MSM Shahla <maryamshahla465@gmail.com>
2026-09-11 17:13:02 +0530

    added comments to index.js

--- src/index.js
+++ src/index.js
@@ -2,16 +2,21 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 
-const app = express();
+//creates the express application
+const app = express(); 
+//it uses the port defined in the environment variable PORT or defaults to 5000 if not defined    
 const PORT = process.env.PORT || 5000;
 
+//allows communication between frontend and backend of different origins
 app.use(cors());
+//allows the express application to convert incoming JSON data to javascript objects
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.json({ message: 'WeatherWise API is running'});
 });
 
+//Starts the backend server and makes it listen for requests on the specified PORT.
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
